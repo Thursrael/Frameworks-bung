7ca8e9cf149d8832772e07037a81c84452e96ef1
RuxueSha <74711423+RuxueSha@users.noreply.github.com>
2020-12-28 15:51:55 +0100

    Add files via upload

---
+++ src/Controller/RestaurantlisteController.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Cake\Core\Configure;
+use Cake\Http\Exception\ForbiddenException;
+use Cake\Http\Exception\NotFoundException;
+use Cake\Http\Response;
+use Cake\View\Exception\MissingTemplateException;
+
+
+
+class RestaurantlisteController extends AppController
+{
+
+    public function index()
+    {
+        
+        
+                
+    }
+}
